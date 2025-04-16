9c051c7760e7a32e1a0a22c6dd77d6eeaf2a38b5
MishaalTeeluck <mishaal.s.teeluck@accenture.com>
2025-04-16 11:29:37 +0400

    feat(main-page): implement MainPage component to structure inner layout

--- src/components/App/App.component.tsx
+++ src/components/App/App.component.tsx
@@ -1,12 +1,16 @@
+import { Box } from '@chakra-ui/react';
 import { Footer } from '../Footer/Footer.component';
 import Header from '../Header/Header.component';
 import './App.component.css';
+import { MainComponent } from '../Main/Main.component';
 
 function App() {
   return (
     <>
       <Header />
-      <main></main>
+      <Box as='main' flex='1' padding='12'>
+        <MainComponent />
+      </Box>
       <Footer />
     </>
   );
