995c99973cbf4affbec51c69d08ce0b5d2ca68bb
MishaalTeeluck <mishaal.s.teeluck@accenture.com>
2025-04-17 13:55:59 +0400

    refactor(router): restructure application routing configuration

--- src/components/App/App.component.tsx
+++ src/components/App/App.component.tsx
@@ -1,19 +1,8 @@
-import { Box } from '@chakra-ui/react';
-import { Footer } from '../Footer/Footer.component';
-import Header from '../Header/Header.component';
 import './App.component.css';
 import { MainComponent } from '../Main/Main.component';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Box as='main' flex='1' padding='12'>
-        <MainComponent />
-      </Box>
-      <Footer />
-    </>
-  );
+  return <MainComponent />;
 }
 
 export default App;
