3765c4062d3cfca8a617ae0bf71141cbe4d8a9ee
MishaalTeeluck <mishaal.s.teeluck@accenture.com>
2025-04-27 12:36:56 +0400

    feat(routing): update History page to support dynamic routing

--- src/routes/route.config.tsx
+++ src/routes/route.config.tsx
@@ -1,16 +1,17 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "../components/App/App.component";
-import Layout from "./route.layout";
-import { UploadFileComponent } from "../components/Tools/TestPlanGenerator/UploadFile/UploadFile.component";
-import { ExcelViewerComponent } from "../components/Tools/TestPlanGenerator/ExcelViewer/ExcelViewer.component";
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../components/App/App.component';
+import Layout from './route.layout';
+import { UploadFileComponent } from '../components/Tools/TestPlanGenerator/UploadFile/UploadFile.component';
+import { ExcelViewerComponent } from '../components/Tools/TestPlanGenerator/ExcelViewer/ExcelViewer.component';
+import { HistoryComponent } from '../components/Tools/TestPlanGenerator/History/History.component';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Layout />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <App />,
       },
       {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
       {
         path: '/tools/testplangenerator/result',
         element: <ExcelViewerComponent />,
+      },
+      {
+        path: '/tools/testplangenerator/history',
+        element: <HistoryComponent />,
       },
     ],
   },
